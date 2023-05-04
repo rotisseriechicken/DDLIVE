0957ce2800c747fe7379c3a51db94394e6fce98e
ROTISSERIE CHICKEN <98714749+rotisseriechicken@users.noreply.github.com>
2023-05-04 00:35:02 -0500

    Leaderboard Gamepage Update

---
+++ proxyPost.php
@@ -0,0 +1,19 @@
+<?php header('Access-Control-Allow-Origin: *');
+
+$userid = $_GET['userid'];
+$url = 'http://dd.hasmodai.com/backend16/get_replay.php';
+$data = array('replay' => $userid);
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* ERROR */ }
+
+print_r($result);
+?>
